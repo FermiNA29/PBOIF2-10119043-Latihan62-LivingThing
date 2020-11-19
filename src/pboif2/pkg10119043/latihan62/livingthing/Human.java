6956ae07eaa53318f98dba2ab3b5aea710cbb920
FermiNA29 <58778153+FermiNA29@users.noreply.github.com>
2020-11-19 18:52:05 +0700

    Living Thing

---
+++ src/pboif2/pkg10119043/latihan62/livingthing/Human.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119043.latihan62.livingthing;
+
+/**
+ *
+ * @author FERMI
+ * NAMA      : FERMI NAUFAL AKBAR
+ * NIM       : 10119043
+ * KELAS     : IF2
+ * DESKRIPSI : Program Living Thing
+ */
+public class Human extends LivingThing{
+    private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    @Override
+    public void walk(String nama) {
+        System.out.println(nama + " sedang berjalan");
+    }
+    
+}
---
+++ src/pboif2/pkg10119043/latihan62/livingthing/PBOIF210119043Latihan62LivingThing.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119043.latihan62.livingthing;
+
+/**
+ *
+ * @author FERMI
+ * NAMA      : FERMI NAUFAL AKBAR
+ * NIM       : 10119043
+ * KELAS     : IF2
+ * DESKRIPSI : Program Living Thing
+ */
+public class PBOIF210119043Latihan62LivingThing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Human human = new Human();
+        human.setNama("Rizki Adam Kurniawan");
+        human.walk(human.getNama());
+        human.breath(human.getNama());
+        human.eat(human.getNama());
+    }
+    
+}
